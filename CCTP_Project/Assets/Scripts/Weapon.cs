eb077918469ca2a952cae774905f9f5f0a7343b1
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-11-22 12:28:59 +0000

    bullet trails always work
    add way to toggle them on and off now and add material to the trail prefab

--- CCTP_Project/Assets/Scripts/Weapon.cs
+++ CCTP_Project/Assets/Scripts/Weapon.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 using Cinemachine;
+using UnityEngine.InputSystem.HID;
+using JetBrains.Annotations;
 
 public class Weapon : MonoBehaviour
 {
@@ -25,6 +27,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Vector3 BulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
     [SerializeField] private TrailRenderer BulletTrail;
     [SerializeField] private float bulletSpeed = 100f;
+    EnemyHealth target;
 
     bool canShoot = true;
 
@@ -83,30 +86,19 @@ public class Weapon : MonoBehaviour
     {
         Vector3 direction = GetDirection();
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position /*BulletSpawnPoint.position*/ ,  direction, out hit, range, Mask))
+        
+        TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+        if (Physics.Raycast(FPCamera.transform.position /*BulletSpawnPoint.position*/ ,  direction, out hit, range, Mask))
         {
-            
-            // change all references to 'FPCamera.transform.position', to BulletSpawnPoint.Position. (Private Transform BulletSpawnPoint)
-            TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
-            StartCoroutine(SpawnTrail(trail, hit.point));
-
-            //CreateHitImpact(hit);
-            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if (target == null)
-            {
-                CreateHitImpact(hit);
-                return;
-            }
-            else
-            {
-                CreateEnemyHitImpact(hit);
-                target.TakeDamage(damage);
-            }
-            
+            target = hit.transform.GetComponent<EnemyHealth>();
+            //EnemyHealth HitEnemy = hit.transform.GetComponent<EnemyHealth>();
+            StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
         }
         else
         {
-            return;
+            target = null;
+            //EnemyHealth HitEnemy = hit.transform.GetComponent<EnemyHealth>();
+            StartCoroutine(SpawnTrail(trail, BulletSpawnPoint.position + direction * 100, hit.normal, false));
         }
     }
 
@@ -126,12 +118,13 @@ public class Weapon : MonoBehaviour
         return direction;
     }
 
-    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint)
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint, Vector3 HitNormal, bool madeImpact)
     {
         //float time = 0f;
         Vector3 startPosition = Trail.transform.position;
         float distance = Vector3.Distance(Trail.transform.position, HitPoint);
         float startingDistance = distance;
+
 
         while(distance > 0)
         {
@@ -142,19 +135,45 @@ public class Weapon : MonoBehaviour
         }
 
         Trail.transform.position = HitPoint;
+
+        if(madeImpact) 
+        {
+            //CreateHitImpact(HitPoint, HitNormal);
+            ChooseImpact(HitPoint, HitNormal);
+        }
+        else
+        {
+            yield return null;//StartCoroutine(SpawnTrail(Trail, HitPoint, HitNormal, false));
+        }
         //ChooseHitImpact(Hit);
 
         Destroy(Trail.gameObject, Trail.time);
     }
 
-    private void CreateHitImpact(RaycastHit hit)
+    private void ChooseImpact(Vector3 HitPoint, Vector3 HitNormal) 
     {
-        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        //CreateHitImpact(hit);
+        //EnemyHealth target =  //hit.transform.GetComponent<EnemyHealth>();
+        if (target == null)
+        {
+            CreateHitImpact(HitPoint, HitNormal);
+            return;
+        }
+        else
+        {
+            CreateEnemyHitImpact(HitPoint, HitNormal);
+            target.TakeDamage(damage);
+        }
+    }
+
+    private void CreateHitImpact(Vector3 HitPoint, Vector3 HitNormal)
+    {
+        GameObject impact = Instantiate(hitEffect, HitPoint, Quaternion.LookRotation(HitNormal));
         Destroy(impact, 1);
     }
-    private void CreateEnemyHitImpact(RaycastHit hit)
+    private void CreateEnemyHitImpact(Vector3 HitPoint, Vector3 HitNormal)
     {
-        GameObject impact = Instantiate(hitEnemyEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        GameObject impact = Instantiate(hitEnemyEffect, HitPoint, Quaternion.LookRotation(HitNormal));
         Destroy(impact, 1);
     }
 }
