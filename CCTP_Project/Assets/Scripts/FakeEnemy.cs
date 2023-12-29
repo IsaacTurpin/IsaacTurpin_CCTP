93bdbca1911ad23bff12d9b99d6d853b67d0b8d6
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-12-29 14:55:05 +0000

    Basic firing range

---
+++ CCTP_Project/Assets/Scripts/FakeEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeEnemy : MonoBehaviour
+{
+    public void OnDamageTaken()
+    {
+        return;
+    }
+}
