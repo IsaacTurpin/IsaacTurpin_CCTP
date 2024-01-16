42942d6e9c83f1e50a7ac7eb11a96d34e4b4c7c5
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-01-16 15:33:48 +0000

    Enemy health bars
    find way to not have them default active when spawning in

--- CCTP_Project/Assets/Scripts/EnemyHealth.cs
+++ CCTP_Project/Assets/Scripts/EnemyHealth.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
+    [SerializeField] Image healthBarFill;
+    [SerializeField] private float startingHealth;
+
+    GameObject HealthBarEnabled;
+    [SerializeField] GameObject HealthBar;
+    private bool HealthBarActive = true;
 
     bool isDead = false;
 
@@ -14,6 +21,29 @@ public class EnemyHealth : MonoBehaviour
     private void Start()
     {
         enemyCollider = GetComponent<CapsuleCollider>();
+        startingHealth = hitPoints;
+        HealthBarEnabled = GameObject.FindGameObjectWithTag("EnemyHealthBar");
+    }
+
+    private void Update()
+    {
+        if(HealthBarEnabled == null)
+        {
+            HealthBarEnabled = GameObject.FindGameObjectWithTag("EnemyHealthBar");
+        }
+        if(HealthBarEnabled != null)
+        {
+            HealthBarActive = HealthBarEnabled.activeInHierarchy;
+        }
+        
+        if(!HealthBarActive)
+        {
+            HealthBar.SetActive(false);
+        }
+        else
+        {
+            HealthBar.SetActive(true);
+        }
     }
 
     public bool IsDead()
@@ -25,10 +55,23 @@ public class EnemyHealth : MonoBehaviour
     {
         BroadcastMessage("OnDamageTaken");
         hitPoints -= damage;
-        if(hitPoints <= 0)
+        UpdateHealthBar();
+        if (hitPoints <= 0)
         {
             Die();
         }
+    }
+
+    public void UpdateHealthBar()
+    {
+        if(HealthBarActive)
+        {
+            if (healthBarFill != null)
+            {
+                healthBarFill.fillAmount = hitPoints / startingHealth;
+            }
+        }
+
     }
 
     private void Die()
