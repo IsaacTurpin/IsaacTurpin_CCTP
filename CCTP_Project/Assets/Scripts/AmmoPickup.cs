0d70878440cf8f3b8cac4307a249718ee2099350
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-02-02 11:18:51 +0000

    Pickups Juice
    Oscillator, Point Light, Particles

--- CCTP_Project/Assets/Scripts/AmmoPickup.cs
+++ CCTP_Project/Assets/Scripts/AmmoPickup.cs
@@ -6,12 +6,15 @@ public class AmmoPickup : MonoBehaviour
 {
     [SerializeField] int ammoAmount = 5;
     [SerializeField] AmmoType ammoType;
+    [SerializeField] ParticleSystem particleSystem;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+            particleSystem.gameObject.transform.SetParent(null);
+            particleSystem.Play();
             Destroy(gameObject);
         }
     }
