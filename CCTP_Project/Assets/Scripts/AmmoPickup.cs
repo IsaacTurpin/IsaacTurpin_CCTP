e4728ef4d7d3c4895936ac621ccd80c3a94b9d5c
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-02-02 12:35:13 +0000

    Pickups toggle menu

--- CCTP_Project/Assets/Scripts/AmmoPickup.cs
+++ CCTP_Project/Assets/Scripts/AmmoPickup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class AmmoPickup : MonoBehaviour
 {
@@ -8,14 +9,85 @@ public class AmmoPickup : MonoBehaviour
     [SerializeField] AmmoType ammoType;
     [SerializeField] ParticleSystem particleSystem;
 
+    //Toggles
+    [SerializeField] GameObject oscillatorToggle;
+    [SerializeField] GameObject pointLightToggle;
+    [SerializeField] GameObject particleToggle;
+    private bool particlesEnabled;
+    [SerializeField] GameObject meshToggle;
+
+    Oscillator oscillator;
+    [SerializeField] GameObject pointLight;
+    [SerializeField] GameObject realMesh;
+    [SerializeField] GameObject fakeMesh;
+
+    private void Start()
+    {
+        oscillator = GetComponent<Oscillator>();
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
-            particleSystem.gameObject.transform.SetParent(null);
-            particleSystem.Play();
+            if(particlesEnabled)
+            {
+                particleSystem.gameObject.transform.SetParent(null);
+                particleSystem.Play();
+            }
             Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        CheckToggles();
+    }
+
+    private void CheckToggles()
+    {
+        //oscillator toggle
+        if(oscillatorToggle.activeInHierarchy)
+        {
+            oscillator.enabled = true;
+        }
+        else
+        {
+            oscillator.enabled = false;
+        }
+
+        //point light toggle
+        if(pointLightToggle.activeInHierarchy) 
+        {
+            pointLight.SetActive(true);
+        }
+        else
+        {
+            pointLight.SetActive(false);
+        }
+
+        //Particle toggle
+        if(particleToggle.activeInHierarchy)
+        {
+            particlesEnabled = true;
+        }
+        if(!particleToggle.activeInHierarchy)
+        {
+            particlesEnabled = false;
+        }
+
+        //Mesh toggle
+        if(meshToggle.activeInHierarchy)
+        {
+            realMesh.SetActive(true);
+            fakeMesh.SetActive(false);
+        }
+        if(!meshToggle.activeInHierarchy)
+        {
+            realMesh.SetActive(false);
+            fakeMesh.SetActive(true);
         }
     }
 }
